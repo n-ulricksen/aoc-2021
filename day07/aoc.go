bb814cdce98960575b811201ffe7d9c00b02e2ca
Nicholas Ulricksen <n.ulricksen@gmail.com>
2021-12-09 10:01:38 -0800

    day07 solution

---
+++ day07/aoc.go
@@ -0,0 +1,88 @@
+package day00
+
+import (
+	"math"
+
+	"github.com/n-ulricksen/aoc-2021/util"
+)
+
+func part1(input string) int {
+	positions := util.ParseNums(input)
+	posCounts := make(map[int]int)
+	maxPos := 0
+	for _, pos := range positions {
+		maxPos = max(maxPos, pos)
+		posCounts[pos]++
+	}
+
+	var fuelCosts []int
+	var fuelCost, distance int
+	for i := 0; i <= maxPos; i++ {
+		fuelCost = 0
+		for pos, count := range posCounts {
+			distance = abs(pos - i)
+			fuelCost += distance * count
+		}
+		fuelCosts = append(fuelCosts, fuelCost)
+	}
+
+	minFuel := math.MaxInt64
+	for _, cost := range fuelCosts {
+		minFuel = min(minFuel, cost)
+	}
+
+	return minFuel
+}
+
+func part2(input string) int {
+	positions := util.ParseNums(input)
+	posCounts := make(map[int]int)
+	maxPos := 0
+	for _, pos := range positions {
+		maxPos = max(maxPos, pos)
+		posCounts[pos]++
+	}
+
+	var fuelCosts []int
+	var fuelCost, distance int
+	for i := 0; i <= maxPos; i++ {
+		fuelCost = 0
+		for pos, count := range posCounts {
+			distance = abs(pos - i)
+			fuelCost += sumFirstN(distance) * count
+		}
+		fuelCosts = append(fuelCosts, fuelCost)
+	}
+
+	minFuel := math.MaxInt64
+	for _, cost := range fuelCosts {
+		minFuel = min(minFuel, cost)
+	}
+
+	return minFuel
+}
+
+func abs(x int) int {
+	if x < 0 {
+		return -x
+	}
+	return x
+}
+
+func max(x, y int) int {
+	if x > y {
+		return x
+	}
+	return y
+}
+
+func min(x, y int) int {
+	if x < y {
+		return x
+	}
+	return y
+}
+
+func sumFirstN(n int) int {
+	return (n * (n + 1)) / 2
+}
---
+++ day07/aoc_test.go
@@ -0,0 +1,45 @@
+package day00
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"testing"
+)
+
+const inputPath = "./input.txt"
+
+const sInput = `16,1,2,0,4,2,7,1,2,14`
+
+const sExpected = 37
+const sExpected2 = 168
+
+var input string
+
+func init() {
+	data, err := os.ReadFile(inputPath)
+	if err != nil {
+		log.Fatal(err)
+	}
+	input = string(data)
+}
+
+func TestPart1(t *testing.T) {
+	sGot := part1(sInput)
+	if sGot != sExpected {
+		t.Fatalf("got %d, want %d\n", sGot, sExpected)
+	}
+
+	result := part1(input)
+	fmt.Println(result)
+}
+
+func TestPart2(t *testing.T) {
+	sGot := part2(sInput)
+	if sGot != sExpected2 {
+		t.Fatalf("got %d, want %d\n", sGot, sExpected2)
+	}
+
+	result := part2(input)
+	fmt.Println(result)
+}
