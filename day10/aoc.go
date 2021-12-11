bbfcae62b176c7367dc6a3f225f2cb64f3c09b36
Nicholas Ulricksen <n.ulricksen@gmail.com>
2021-12-11 07:43:33 -0800

    day10 solution

---
+++ day10/aoc.go
@@ -0,0 +1,103 @@
+package day00
+
+import (
+	"log"
+	"sort"
+	"strings"
+)
+
+var syntaxErrorScore = map[rune]int{
+	')': 3,
+	']': 57,
+	'}': 1197,
+	'>': 25137,
+}
+
+var syntaxAutocompleteScore = map[rune]int{
+	')': 1,
+	']': 2,
+	'}': 3,
+	'>': 4,
+}
+
+var matchingSymbol = map[rune]rune{
+	'(': ')',
+	'[': ']',
+	'{': '}',
+	'<': '>',
+}
+
+func part1(input string) int {
+	input = strings.TrimSpace(input)
+	lines := strings.Split(input, "\n")
+
+	var errorSum int
+	for _, line := range lines {
+		var stack []rune
+	CheckSymbol:
+		for _, symbol := range line {
+			switch symbol {
+			case '(', '[', '{', '<':
+				stack = append(stack, symbol)
+			case ')', ']', '}', '>':
+				expected := matchingSymbol[stack[len(stack)-1]]
+				if symbol != expected {
+					errorSum += syntaxErrorScore[symbol]
+					break CheckSymbol
+				} else {
+					stack = stack[:len(stack)-1]
+				}
+			default:
+				log.Fatal("unsopported character:", symbol)
+			}
+		}
+	}
+
+	return errorSum
+}
+
+func part2(input string) int {
+	input = strings.TrimSpace(input)
+	lines := strings.Split(input, "\n")
+
+	var completionScores []int
+	for _, line := range lines {
+		var stack []rune
+		var corrupted bool
+	CheckSymbol:
+		for _, symbol := range line {
+			switch symbol {
+			case '(', '[', '{', '<':
+				stack = append(stack, symbol)
+			case ')', ']', '}', '>':
+				expected := matchingSymbol[stack[len(stack)-1]]
+				if symbol != expected {
+					// Corrupted line
+					corrupted = true
+					break CheckSymbol
+				} else {
+					stack = stack[:len(stack)-1]
+				}
+			default:
+				log.Fatal("unsopported character:", symbol)
+			}
+		}
+
+		if !corrupted && len(stack) > 0 {
+			score := 0
+			// Autocomplete line
+			for len(stack) > 0 {
+				symbol := stack[len(stack)-1]
+				stack = stack[:len(stack)-1]
+				match := matchingSymbol[symbol]
+				score = score*5 + syntaxAutocompleteScore[match]
+			}
+			completionScores = append(completionScores, score)
+		}
+	}
+
+	sort.Ints(completionScores)
+	midIdx := len(completionScores) / 2
+
+	return completionScores[midIdx]
+}
---
+++ day10/aoc_test.go
@@ -0,0 +1,54 @@
+package day00
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"testing"
+)
+
+const inputPath = "./input.txt"
+
+const sInput = `[({(<(())[]>[[{[]{<()<>>
+[(()[<>])]({[<{<<[]>>(
+{([(<{}[<>[]}>{[]{[(<()>
+(((({<>}<{<{<>}{[]{[]{}
+[[<[([]))<([[{}[[()]]]
+[{[{({}]{}}([{[{{{}}([]
+{<[[]]>}<{[{[{[]{()[[[]
+[<(<(<(<{}))><([]([]()
+<{([([[(<>()){}]>(<<{{
+<{([{{}}[<[[[<>{}]]]>[]]`
+
+const sExpected = 26397
+const sExpected2 = 288957
+
+var input string
+
+func init() {
+	data, err := os.ReadFile(inputPath)
+	if err != nil {
+		log.Fatal(err)
+	}
+	input = string(data)
+}
+
+func TestPart1(t *testing.T) {
+	sGot := part1(sInput)
+	if sGot != sExpected {
+		t.Fatalf("got %d, want %d\n", sGot, sExpected)
+	}
+
+	result := part1(input)
+	fmt.Println(result)
+}
+
+func TestPart2(t *testing.T) {
+	sGot := part2(sInput)
+	if sGot != sExpected2 {
+		t.Fatalf("got %d, want %d\n", sGot, sExpected2)
+	}
+
+	result := part2(input)
+	fmt.Println(result)
+}
